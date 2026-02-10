8802a1e68080382b7383a9648fbf390941c9683e
Shraddhanjali Paikray <shraddhanjalipaikray@gmail.com>
2026-02-10 15:39:28 +0530

    Add dashboard and submit complaint pages
    Created user dashboard and complaint submission UI and configured routing

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard(){
+    return(
+        <div>
+            <h2>User Dashboard</h2>
+            <p>Your submitted complaints will appear here.</p>
+        </div>
+    );
+}
+
+export default Dashboard;
