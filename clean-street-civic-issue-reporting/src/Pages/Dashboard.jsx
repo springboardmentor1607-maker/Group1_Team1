e35d2433458761a852701d71ca5a024a4391c354
Nithyasri26 <nithyasridevi26@gmail.com>
2026-02-10 20:15:59 +0530

    Initial commit of my project folder

---
+++ clean-street-civic-issue-reporting/src/Pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard(){
+    return <h2>User Dashbord</h2>;
+
+}
+export default Dashboard;
---
+++ clean-street-civic-issue-reporting/src/Pages/EditProfile.jsx
@@ -0,0 +1,5 @@
+function EditProfile(){
+    return <h2>Edit Profile</h2>;
+}
+
+export default EditProfile;
