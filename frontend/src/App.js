f3a0821e3601ea7b58353d775e1b4e5bbfed9102
Shraddhanjali Paikray <shraddhanjalipaikray@gmail.com>
2026-02-13 22:17:21 +0530

    improvised the ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import SubmitComplaint from "./pages/SubmitComplaint";
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <Routes>
         {/* Redirect home to dashboard */}
         <Route path="/" element={<Navigate to="/dashboard" />} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/submit-complaint" element={<SubmitComplaint />} />
       </Routes>
