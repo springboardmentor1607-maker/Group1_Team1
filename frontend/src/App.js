c0aa6a6ef63fb58169d28bd1d022407c027f4bb4
Shraddhanjali Paikray <shraddhanjalipaikray@gmail.com>
2026-02-14 23:24:33 +0530

    ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,27 @@ import Dashboard from "./pages/Dashboard";
 import SubmitComplaint from "./pages/SubmitComplaint";
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Profile from "./pages/Profile";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Redirect home to dashboard */}
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Redirect root to login */}
+        <Route path="/"                 element={<Navigate to="/login" replace />} />
+
+        {/* Auth pages */}
+        <Route path="/login"            element={<Login />} />
+        <Route path="/signup"           element={<Signup />} />
+
+        {/* App pages */}
+        <Route path="/dashboard"        element={<Dashboard />} />
+        <Route path="/profile"          element={<Profile />} />
         <Route path="/submit-complaint" element={<SubmitComplaint />} />
+
+        {/* Catch-all fallback */}
+        <Route path="*"                 element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
