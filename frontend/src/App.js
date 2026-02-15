dbc46e3dc521d51660e59b9cbaa146e02155f186
Rasmi04 <rasmimahanandi@gmail.com>
2026-02-15 14:54:08 +0530

    Proile page with logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,6 @@ import Profile from "./pages/Profile";
 
 function App() {
   return <Profile />;
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard";
-import SubmitComplaint from "./pages/SubmitComplaint";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-
-import "./App.css";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Redirect home to dashboard */}
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/submit-complaint" element={<SubmitComplaint />} />
-      </Routes>
-    </Router>
-  );
 }
 
 export default App;
