52c0728287abc8ad187111bf8a8232e2ff95d1fc
anusha <venkataanusha681@gmail.com>
2026-02-15 12:34:50 +0530

    changed app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import SubmitComplaint from "./pages/SubmitComplaint";
+
+
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Profile from "./pages/Profile";
+
 
 export default function App() {
   return (
@@ -17,9 +17,7 @@ export default function App() {
         <Route path="/signup"           element={<Signup />} />
 
         {/* App pages */}
-        <Route path="/dashboard"        element={<Dashboard />} />
-        <Route path="/profile"          element={<Profile />} />
-        <Route path="/submit-complaint" element={<SubmitComplaint />} />
+
 
         {/* Catch-all fallback */}
         <Route path="*"                 element={<Navigate to="/login" replace />} />
