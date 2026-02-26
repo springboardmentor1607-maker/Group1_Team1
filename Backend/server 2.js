47fba951cc042136f3e4f84538558f001e7b0433
Shraddhanjali Paikray <shraddhanjalipaikray@gmail.com>
2026-02-26 13:22:10 +0530

    redundant file removed

---
+++ Backend/server 2.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config({ path: "./backend.env" });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/user"));
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Clean Street API running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
