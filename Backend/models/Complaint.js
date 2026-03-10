639583510fc4291e88c7069088ec5189f5e550fa
anusha <venkataanusha681@gmail.com>
2026-03-10 20:26:24 +0530

    updated the ui part

--- Backend/models/Complaint.js
+++ Backend/models/Complaint.js
@@ -65,7 +65,7 @@ const complaintSchema = new mongoose.Schema(
 
     status: {
       type: String,
-      enum: ["received", "in_review", "resolved", "assigned", "completed", "pending"],
+      enum: ["received", "in_review", "resolved"],
       default: "received",
     },
 
